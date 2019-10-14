ac0fa87e441e499078bc1ce60b98b0bf4fb3643b
Tim Buckley <timothypbuckley@gmail.com>
2019-10-14 13:26:34 -0400

    Bump version to 0.8.3
    `0.8.3` to include PR #46

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Forcex.Mixfile do
   def project do
     [
       app: :forcex,
-      version: "0.8.2",
+      version: "0.8.3",
       elixir: "~> 1.5",
       name: "Forcex",
       description: @description,
